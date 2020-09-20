27b3a2d6f70d5afb44a1a1e6aa08cb13411ed0a1
Gustavo Batistuti <gustavo.batistuti@bandtec.com.br>
2020-09-20 20:23:56 -0300

    Include health-check route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,16 @@
-import { Router } from 'express'
+import { Request, Response, Router } from "express";
 
-const routes = Router()
+const routes = Router();
 
-routes.get('/', (_, res) => { return res.json({ message: "Hello, busiBoxer :)" }) })
+routes.get("/", (_, res) => {
+  return res.json({ message: "Hello, busiBoxer :))" });
+});
 
-module.exports = routes
+routes.get("/health-check", (_: Request, res: Response) => {
+  return res.json({
+    uptime: process.uptime(),
+    message: "OK",
+  });
+});
+
+module.exports = routes;
